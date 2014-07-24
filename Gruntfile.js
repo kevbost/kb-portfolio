e79e52feae7e8170f29ba571cb5ad8da94765c4f
Kevin Bost <kevbost@gmail.com>
2014-07-24 11:00:44 -0400

    icon test

--- Gruntfile.js
+++ Gruntfile.js
@@ -328,12 +328,6 @@ module.exports = function (grunt) {
           cwd: '.',
           src: 'bower_components/bootstrap-sass-official/vendor/assets/fonts/bootstrap/*',
           dest: '<%= config.dist %>'
-        }, {
-          expand: true,
-          dot: true,
-          cwd: '.',
-          src: 'bower_components/monosocialiconsfont/*',
-          dest: '<%= config.dist %>'
         }]
       },
       styles: {
